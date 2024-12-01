e2ffb143a17a68d47f82afe172d85c61d724a514
ManjuRam K.H. <68156394+manjuram07@users.noreply.github.com>
2024-12-01 10:02:57 +0530

    Update CustomerAPI.java

--- Ekart_Project/CustomerMS/src/main/java/com/infy/ekart/customer/api/CustomerAPI.java
+++ Ekart_Project/CustomerMS/src/main/java/com/infy/ekart/customer/api/CustomerAPI.java
@@ -1,10 +1,9 @@
 package com.infy.ekart.customer.api;
 
-
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -35,93 +34,78 @@ import jakarta.validation.constraints.Pattern;
 @Validated
 public class CustomerAPI {
 
-	@Autowired
-	private CustomerService customerService;
+    @Autowired
+    private CustomerService customerService;
 
-	@Autowired
-	private RestTemplate template;
+    @Autowired
+    @LoadBalanced
+    private RestTemplate restTemplate;
 
-	@Autowired
-	private Environment environment;
+    @Autowired
+    private Environment environment;
 
-	static Log logger = LogFactory.getLog(CustomerAPI.class);
+    static Log logger = LogFactory.getLog(CustomerAPI.class);
 
-	@PostMapping(value = "/login")
-	public ResponseEntity<CustomerDTO> authenticateCustomer(@Valid @RequestBody CustomerDTO customerDTO)
-			throws EKartCustomerException {
+    @PostMapping(value = "/login")
+    public ResponseEntity<CustomerDTO> authenticateCustomer(@Valid @RequestBody CustomerDTO customerDTO)
+            throws EKartCustomerException {
 
-		logger.info("CUSTOMER TRYING TO LOGIN, VALIDATING CREDENTIALS. CUSTOMER EMAIL ID: " + customerDTO.getEmailId());
-		CustomerDTO customerDTOFromDB = customerService.authenticateCustomer(customerDTO.getEmailId(),
-				customerDTO.getPassword());
-		logger.info("CUSTOMER LOGIN SUCCESS, CUSTOMER EMAIL : " + customerDTOFromDB.getEmailId());
-		return new ResponseEntity<>(customerDTOFromDB, HttpStatus.OK);
-	}
+        logger.info("CUSTOMER TRYING TO LOGIN, VALIDATING CREDENTIALS. CUSTOMER EMAIL ID: " + customerDTO.getEmailId());
+        CustomerDTO customerDTOFromDB = customerService.authenticateCustomer(customerDTO.getEmailId(),
+                customerDTO.getPassword());
+        logger.info("CUSTOMER LOGIN SUCCESS, CUSTOMER EMAIL : " + customerDTOFromDB.getEmailId());
+        return new ResponseEntity<>(customerDTOFromDB, HttpStatus.OK);
+    }
 
-	@PostMapping(value = "/register")
-	public ResponseEntity<String> registerCustomer(@Valid @RequestBody CustomerDTO customerDTO)
-			throws EKartCustomerException {
+    @PostMapping(value = "/register")
+    public ResponseEntity<String> registerCustomer(@Valid @RequestBody CustomerDTO customerDTO)
+            throws EKartCustomerException {
 
-		logger.info("CUSTOMER TRYING TO REGISTER. CUSTOMER EMAIL ID: " + customerDTO.getEmailId());
-		String registeredWithEmailID = customerService.registerNewCustomer(customerDTO);
-		registeredWithEmailID = environment.getProperty("CustomerAPI.CUSTOMER_REGISTRATION_SUCCESS")
-				+ registeredWithEmailID;
-		return new ResponseEntity<>(registeredWithEmailID, HttpStatus.OK);
-	}
+        logger.info("CUSTOMER TRYING TO REGISTER. CUSTOMER EMAIL ID: " + customerDTO.getEmailId());
+        String registeredWithEmailID = customerService.registerNewCustomer(customerDTO);
+        registeredWithEmailID = environment.getProperty("CustomerAPI.CUSTOMER_REGISTRATION_SUCCESS")
+                + registeredWithEmailID;
+        return new ResponseEntity<>(registeredWithEmailID, HttpStatus.OK);
+    }
 
-	@PutMapping(value = "/customer/{customerEmailId:.+}/address/")
-	public ResponseEntity<String> updateShippingAddress(
-			@Pattern(regexp = "[a-zA-Z0-9._]+@[a-zA-Z]{2,}\\.[a-zA-Z][a-zA-Z.]+", message = "{invalid.email.format}") String customerEmailId,
-			@RequestBody String address) throws EKartCustomerException {
+    @PutMapping(value = "/customer/{customerEmailId:.+}/address/")
+    public ResponseEntity<String> updateShippingAddress(
+            @Pattern(regexp = "[a-zA-Z0-9._]+@[a-zA-Z]{2,}\\.[a-zA-Z][a-zA-Z.]+", message = "{invalid.email.format}") String customerEmailId,
+            @RequestBody String address) throws EKartCustomerException {
 
-		customerService.updateShippingAddress(customerEmailId, address);
-		String modificationSuccessMsg = environment.getProperty("CustomerAPI.UPDATE_ADDRESS_SUCCESS");
-		return new ResponseEntity<>(modificationSuccessMsg, HttpStatus.OK);
+        customerService.updateShippingAddress(customerEmailId, address);
+        String modificationSuccessMsg = environment.getProperty("CustomerAPI.UPDATE_ADDRESS_SUCCESS");
+        return new ResponseEntity<>(modificationSuccessMsg, HttpStatus.OK);
+    }
 
-	}
+    @DeleteMapping(value = "/customer/{customerEmailId:.+}")
+    public ResponseEntity<String> deleteShippingAddress(
+            @Pattern(regexp = "[a-zA-Z0-9._]+@[a-zA-Z]{2,}\\.[a-zA-Z][a-zA-Z.]+", message = "{invalid.email.format}") @PathVariable("customerEmailId") String customerEmailId)
+            throws EKartCustomerException {
 
-	@DeleteMapping(value = "/customer/{customerEmailId:.+}")
-	public ResponseEntity<String> deleteShippingAddress(
-			@Pattern(regexp = "[a-zA-Z0-9._]+@[a-zA-Z]{2,}\\.[a-zA-Z][a-zA-Z.]+", message = "{invalid.email.format}") @PathVariable("customerEmailId") String customerEmailId)
-			throws EKartCustomerException {
+        customerService.deleteShippingAddress(customerEmailId);
+        String modificationSuccessMsg = environment.getProperty("CustomerAPI.CUSTOMER_ADDRESS_DELETED_SUCCESS");
+        return new ResponseEntity<>(modificationSuccessMsg, HttpStatus.OK);
+    }
 
-		customerService.deleteShippingAddress(customerEmailId);
-		String modificationSuccessMsg = environment.getProperty("CustomerAPI.CUSTOMER_ADDRESS_DELETED_SUCCESS");
-		return new ResponseEntity<>(modificationSuccessMsg, HttpStatus.OK);
+    @PostMapping(value = "/customercarts/add-product")
+    public ResponseEntity<String> addProductToCart(@Valid @RequestBody CustomerCartDTO customerCartDTO)
+            throws EKartCustomerException {
 
-	}
+        customerService.getCustomerByEmailId(customerCartDTO.getCustomerEmailId());
 
-	@PostMapping(value = "/customercarts/add-product")
-	public ResponseEntity<String> addProductToCart(@Valid @RequestBody CustomerCartDTO customerCartDTO)
-			throws EKartCustomerException {
+        for (CartProductDTO cartProductDTO : customerCartDTO.getCartProducts()) {
+            String productUrl = "http://ProductMS/Ekart/product-api/product/" + cartProductDTO.getProduct().getProductId();
+            ResponseEntity<ProductDTO> productResponse = restTemplate.getForEntity(productUrl, ProductDTO.class);
 
-		customerService.getCustomerByEmailId(customerCartDTO.getCustomerEmailId());
+            if (!productResponse.getStatusCode().is2xxSuccessful()) {
+                throw new EKartCustomerException("ProductAPI.PRODUCT_NOT_FOUND");
+            }
+        }
 
-		for (CartProductDTO cartProductDTO : customerCartDTO.getCartProducts()) {
+        String cartUrl = "http://CartMS/Ekart/customercart-api/products";
+        ResponseEntity<String> productAddedToCartMessage = restTemplate.postForEntity(cartUrl, customerCartDTO, String.class);
 
-			/*template.getForEntity(
-					"http://localhost:3334/Ekart/product-api/product/" + cartProductDTO.getProduct().getProductId(),
-					ProductDTO.class);*/
-			template.getForEntity(
-					"http://ProductMS/Ekart/product-api/product/" + cartProductDTO.getProduct().getProductId(),
-					ProductDTO.class);
-			
-			// We are calling the product API using hard-coded URI
-			// Replace this call with the appropriate MS name
-			// Product API is upscaled (available in 2 numbers). Hence, use load balanced
-			// template to make call to the Product API
-
-		}
-
-		ResponseEntity<String> productAddedToCartMessage = template
-				.postForEntity("http://CartMS/Ekart/customercart-api/products", customerCartDTO, String.class);
-		// We are calling the Cart API using hard-coded URI
-		// Replace this call with the appropriate MS name
-		// CartMS is not an upscaled one (available in 1 number) , still load-balanced
-		// rest template should be used here to make the call
-		// Don't create and autowire a normal rest template because load balanced
-		// template is already in config file
-
-		return productAddedToCartMessage;
-	}
-
+        return productAddedToCartMessage;
+    }
 }
